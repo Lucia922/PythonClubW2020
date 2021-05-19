466a0989657ca34fcc482110b9b37558eafc7468
Jinhee Byoun <jinheebyoun@Jinhees-MBP.domain>
2021-05-18 18:43:36 -0700

    Meeting detail commit

--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, MeetingMinutes, Resource, Event
 # Create your views here.
 def index(request):
@@ -12,3 +12,6 @@ def meetings(request):
     meeting_list=Meeting.objects.all()
     return render(request, 'club/meetings.html', {'meeting_list': meeting_list})  
 
+def meetingdetail(request, id):
+    meeting=get_object_or_404(Meeting, pk=id)
+    return render(request, 'club/meetingdetail.html', {'meeting' : meeting})
